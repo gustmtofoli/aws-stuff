2b2f7e13029e9a24eed2a0b278dcbb7b2fa192a3
Gustavo <gmtofoli@gmail.com>
2020-02-10 02:00:15 -0300

    database operations templates

--- sqs-system/database/Postgres.py
+++ sqs-system/database/Postgres.py
@@ -5,12 +5,35 @@ class Postgres:
         self.user = ""
         self.password = ""
         self.database = ""
+        self.cursor = None
+        self.connection = None
     
     def connectToPostgres(self, user, password, host, port, database):
         try:
             connection = db.connect(user, password, host, port, database)
             cursor = connection.cursor()
+            self.cursor = cursor
+            self.connection = connection
             return cursor, connection
         except (Exception, db.Error) as error :
             print("Error while connecting to PostgreSQL", error)
             return False
+    
+    def __executeAndCommit(self, query):
+        self.cursor.execute(query)
+        self.connection.commit()
+    
+    def insert(self):
+        query = "insert into table values (...);"
+        self.__executeAndCommit(query)
+    
+    def delete(self):
+        query = "delete from table where ...;"
+        self.__executeAndCommit(query)
+    
+    def select(self):
+        query = "select * from table where ...;"
+        self.__executeAndCommit(query)
+        
+        return self.cursor.fetchall()
+    
--- sqs-system/lambda_function.py
+++ sqs-system/lambda_function.py
@@ -3,14 +3,24 @@ import boto3 as aws
 from database.Rds import Rds
 from database.Postgres import Postgres
 from sqs.Sqs import Sqs
+import os
 
 def lambda_handler(event, context):
     
     rds = Rds()
-    endpoint = rds.getInstance("dbIdentifier")
+    
+    endpoint = rds.getInstance(os.environ['dbIdentifier'])
     
     postgres = Postgres()
-    cursor, connection = postgres.connectToPostgres("user", "password", endpoint.host, endpoint.port, "database")
+    
+    cursor, connection = postgres.connectToPostgres(
+        os.environ['user'], 
+        os.environ['password'], 
+        endpoint.host, 
+        endpoint.port, 
+        os.environ['database']
+    )
+    
     
     sqs = Sqs()
     
